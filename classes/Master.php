1304f9ca42d686b5831c3f225c17ee8a90474866
Leke Abiodun <124153246+a-short-dev@users.noreply.github.com>
2023-11-05 15:52:23 +0100

    added npm packages

--- classes/Master.php
+++ classes/Master.php
@@ -263,6 +263,7 @@ Class Master extends DBConnection {
 			if(!empty($data)) $data .= ", ";
 			$data .= "('{$driver_offense_id}','{$v}','{$fine[$k]}','{$status}','{$date_created}')";
 		}
+    //add the new columns here for due date
 		$save2= $this->conn->query("INSERT INTO `offense_items` (`driver_offense_id`,`offense_id`,`fine`,`status`,`date_created`) VALUES {$data}");
 		$this->capture_err();
 		if($save && $save2){
