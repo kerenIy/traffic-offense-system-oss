65514fccd7982a9bdf954517c1862a14d6995a09
Leke Abiodun <124153246+a-short-dev@users.noreply.github.com>
2023-11-13 09:14:45 +0100

    chore: payment and offense

--- report-offense.php
+++ report-offense.php
@@ -14,6 +14,99 @@
   <meta property="og:image" content="http://localhost/charity/uploads/blog_uploads/banners/4_banner.jpg" />
   <meta property="og:url" content="http://localhost/charity/pages/sample_post.php" />
   <?php require_once('./inc/header.php') ?>
+  <link rel="icon" href="<?php echo validate_image($_settings->info('logo')) ?>" />
+  <!-- Google Font: Source Sans Pro -->
+  <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback"> -->
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <!-- <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> -->
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet"
+    href="<?php echo base_url ?>plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  <!-- Select2 -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/select2/css/select2.min.css">
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url ?>dist/css/adminlte.css">
+  <link rel="stylesheet" href="<?php echo base_url ?>dist/css/custom.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/summernote/summernote-bs4.min.css">
+  <!-- SweetAlert2 -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+  <style type="text/css">
+    /* Chart.js */
+    @keyframes chartjs-render-animation {
+      from {
+        opacity: .99
+      }
+
+      to {
+        opacity: 1
+      }
+    }
+
+    .chartjs-render-monitor {
+      animation: chartjs-render-animation 1ms
+    }
+
+    .chartjs-size-monitor,
+    .chartjs-size-monitor-expand,
+    .chartjs-size-monitor-shrink {
+      position: absolute;
+      direction: ltr;
+      left: 0;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      overflow: hidden;
+      pointer-events: none;
+      visibility: hidden;
+      z-index: -1
+    }
+
+    .chartjs-size-monitor-expand>div {
+      position: absolute;
+      width: 1000000px;
+      height: 1000000px;
+      left: 0;
+      top: 0
+    }
+
+    .chartjs-size-monitor-shrink>div {
+      position: absolute;
+      width: 200%;
+      height: 200%;
+      left: 0;
+      top: 0
+    }
+  </style>
+
+  <!-- jQuery -->
+  <script src="<?php echo base_url ?>plugins/jquery/jquery.min.js"></script>
+  <!-- jQuery UI 1.11.4 -->
+  <script src="<?php echo base_url ?>plugins/jquery-ui/jquery-ui.min.js"></script>
+  <!-- SweetAlert2 -->
+  <script src="<?php echo base_url ?>plugins/sweetalert2/sweetalert2.min.js"></script>
+  <!-- Toastr -->
+  <script src="<?php echo base_url ?>plugins/toastr/toastr.min.js"></script>
+  <script>
+    var _base_url_ = '<?php echo base_url ?>';
+  </script>
+  <script src="<?php echo base_url ?>dist/js/script.js"></script>
+
 </head>
 
 
@@ -35,13 +128,6 @@
             <label for="control-label" for="reporter_phone">Reporter Phone</label>
             <input type="text" class="form-control" id="reporter_phone" placeholder="Enter reporter phone" required />
           </div>
-          <div class="form-group">
-            <label class="control-label" for="status">Status</label>
-            <select name="status" id="status" class="custom-select" required>
-              <option value="0" <?php echo (isset($status) && $status == '0') ? 'selected' : '' ?>>Pending</option>
-              <option value="1" <?php echo (isset($status) && $status == '1') ? 'selected' : '' ?>>Paid</option>
-            </select>
-          </div>
         </div>
 
         <div class="col-6">
@@ -52,35 +138,14 @@
               required>
           </div>
           <div class="form-group">
-            <lable class="control-label" for="date_created">Due Date</lable>
-            <input type="datetime-local" class="form-control" name="due_date" id="due_date"
-              value="<?php echo isset($due_date) ? date("Y-m-d\\TH:i", strtotime($due_date)) : date("Y-m-d\\TH:i") ?>"
-              required>
-          </div>
-          <div class="form-group">
-            <label class="control-label" for="ticket_no">Ticket No.</label>
-            <input type="text" class="form-control" name="ticket_no" id="ticket_no"
-              value="<?php echo isset($ticket_no) ? $ticket_no : '' ?>" required>
-          </div>
-          <div class="form-group">
-            <lable class="control-label" for="driver_id">Driver</lable>
-            <select name="driver_id" id="driver_id" class="custom-select select2" required>
-              <option value=""></option>
-              <?php
-              $driver = $conn->query("SELECT * FROM `drivers_list` order by `name` asc ");
-              while ($row = $driver->fetch_assoc()):
-                ?>
-                <option value="<?php echo $row['id'] ?>" <?php echo (isset($driver_id) && $driver_id == $row['id']) ? 'selected' : '' ?>>[
-                  <?php echo $row['license_id_no'] ?>]
-                  <?php echo ucwords($row['name']) ?>
-                </option>
-              <?php endwhile; ?>
-            </select>
+            <label class="control-label" for="driver_name">Driver name</label>
+            <input type="text" class="form-control" id="driver_name" placeholder="Enter driver_name" required />
           </div>
         </div>
       </div>
+
       <div class="form-group">
-        <button class="btn btn-flat btn-primary" form="report-offense">Search</button>
+        <button class="btn btn-flat btn-primary" form="report-offense">Report </button>
       </div>
     </form>
   </div>
@@ -91,11 +156,51 @@
       var _this = $(this)
       $('.err-msg').remove()
       start_loader();
-      if ($('[name="offense_id[]"]').length <= 0) {
-        alert_toast('Please add atleast 1 offense item first', 'warning')
+      if ($('[name="offense_name"]').length <= 0) {
+        alert_toast('Please enter your name', 'warning')
         end_loader();
         return false;
       }
+
+
+      $.ajax({
+        url: _base_url_ + "classes/Master.php?f=save_public_offense_record",
+        data: new FormData($(this)[0]),
+        cache: false,
+        contentType: false,
+        processData: false,
+        method: 'POST',
+        type: 'POST',
+        dataType: 'json',
+        error: err => {
+          console.log(err)
+          alert_toast("An error occured", 'error');
+          end_loader();
+        },
+        success: function (resp) {
+          if (typeof resp == 'object' && resp.status == 'success') {
+            end_loader();
+            uni_modal("<i class='fa fa-ticket'></i> Driver's Offense Ticket Details", "offenses/view_details.php?id=" + resp.id, 'mid-large')
+            setTimeout(() => {
+              end_loader();
+            }, 500);
+            $('#uni_modal').on('hide.bs.modal', function (e) {
+              location.href = "report-offense.php";
+            })
+          } else if (resp.status == 'failed' && !!resp.msg) {
+            var el = $('<div>')
+            el.addClass("alert alert-danger err-msg").text(resp.msg)
+            _this.prepend(el)
+            el.show('slow')
+            $("html, body").animate({ scrollTop: _this.closest('.card').offset().top }, "fast");
+            end_loader()
+          } else {
+            alert_toast("An error occured", 'error');
+            end_loader();
+            console.log(resp)
+          }
+        }
+      })
     })
   </script>
 
